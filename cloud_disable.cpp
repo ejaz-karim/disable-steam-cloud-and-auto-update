e6f484cd8e24217c5ef024a43ac6cb8ddc364a83
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-04-27 02:08:00 +0100

    Add cloud_disable.cpp

---
+++ cloud_disable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    string input_directory;
+
+    // C:\Program Files (x86)\Steam\userdata\<id>\7\remote is the directory in Windows
+    cout << "Enter directory: ";
+    getline(cin, input_directory);
+    replace(input_directory.begin(), input_directory.end(), '\\', '/');
+
+    string file_directory = input_directory + "/test2.vdf";
+
+    ifstream if_file(file_directory);
+    stringstream buffer;
+    buffer << if_file.rdbuf();
+    string contents = buffer.str();
+
+    regex pattern("\"CloudEnabled\"\t\t\"1\"");
+    string replacement("$1\"CloudEnabled\"\t\t\"0\"");
+    string result = regex_replace(contents, pattern, replacement);
+
+    if_file.close();
+    ofstream of_file(file_directory);
+    of_file << result;
+    of_file.close();
+    return 0;
+}

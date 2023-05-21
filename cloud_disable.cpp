00ebc0af4b44eadd78bbc605431d2ab44af67887
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-05-21 04:37:39 +0100

    Update cloud_disable.cpp

--- cloud_disable.cpp
+++ cloud_disable.cpp
@@ -18,15 +18,30 @@ int main() {
     ifstream if_file(file_directory);
     stringstream buffer;
     buffer << if_file.rdbuf();
-    string contents = buffer.str();
+
+    ifstream library("C:/Program Files (x86)/Steam/steamapps/libraryfolders.vdf");
+    stringstream library_buffer;
+    library_buffer << library.rdbuf();
+
+    string line;
+    while(getline(library_buffer, line)){
+       
+
+    }
+    
+
+
+
 
     regex pattern("\"CloudEnabled\"\t\t\"1\"");
     string replacement("\"CloudEnabled\"\t\t\"0\"");
-    string result = regex_replace(contents, pattern, replacement);
+    string result = regex_replace(buffer.str(), pattern, replacement);
 
     if_file.close();
+
     ofstream of_file(file_directory);
     of_file << result;
     of_file.close();
+
     return 0;
 }

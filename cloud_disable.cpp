1e6ad64fe4a91d86b66c9f33c52cc773de00cccd
jasserbawi <jasser.bawi19@gmail.com>
2023-05-23 04:32:28 +0100

    Updated cloud_disable.cpp

--- cloud_disable.cpp
+++ cloud_disable.cpp
@@ -12,44 +12,66 @@ int main() {
     cout << "Enter directory: ";
     getline(cin, input_directory);
     replace(input_directory.begin(), input_directory.end(), '\\', '/');
-
     string file_directory = input_directory + "/sharedconfig.vdf";
-
-    ifstream if_file(file_directory);
-    stringstream buffer;
-    buffer << if_file.rdbuf();
 
     ifstream library("C:/Program Files (x86)/Steam/steamapps/libraryfolders.vdf");
     stringstream library_buffer;
     library_buffer << library.rdbuf();
 
-
 	stringstream apps_buffer;
-
     string line;
 	bool appsLineReached = false;
     while(getline(library_buffer, line)){
        if (line.find("apps") != string::npos) {
 		appsLineReached = true;
 	   }
-	   if (appsLineReached == true) {
-		apps_buffer << line << endl;
-		if (line.find("}") != string::npos) {
-			appsLineReached = false;
-		}
+	   if (appsLineReached == true && any_of(line.begin(), line.end(), ::isdigit)) {
+		line.erase(remove(line.begin(), line.end(), '\t'), line.end());
+
+        string idLine;
+        for(int i = 1; line[i] != '"'; i++) {
+            idLine += line[i];
+        }
+        apps_buffer << idLine << endl;
 	   }
+       if (line.find("}") != string::npos) {
+        appsLineReached = false;
+        }
     }
     cout << apps_buffer.str() << endl;
 
-    regex pattern("\"CloudEnabled\"\t\t\"1\"");
-    string replacement("\"CloudEnabled\"\t\t\"0\"");
-    string result = regex_replace(buffer.str(), pattern, replacement);
+    ifstream if_file(file_directory);
+    stringstream config_buffer;
 
-    if_file.close();
+    line = "";
+    bool appsBlockReached = false;
 
-    ofstream of_file(file_directory);
-    of_file << result;
-    of_file.close();
+    while (getline(if_file, line)) {
+        if(line.find("apps") != string::npos) {
+            appsBlockReached = true;
+            config_buffer << line << endl;
+            config_buffer << "\t\t\t\t{" << endl;
 
+            string idLine;
+            while(getline(apps_buffer, idLine)) {
+                config_buffer << "\t\t\t\t\t\"" << idLine << "\"" << endl;
+                config_buffer << "\t\t\t\t\t{" << endl;
+                config_buffer << "\t\t\t\t\t\t\"CloudEnabled\"\t\t\"0\"" << endl;
+                config_buffer << "\t\t\t\t\t}" << endl;
+            }
+        }
+        else if (appsBlockReached && line == "\t\t\t\t}") {
+            appsBlockReached = false;
+            config_buffer << line << endl;
+        }
+        else if(appsBlockReached == false){
+            config_buffer << line << endl;
+        }
+    }
+    
+    ofstream outputFile(file_directory);
+    outputFile << config_buffer.str();
+
+    cout << config_buffer.str() << endl;
     return 0;
 }

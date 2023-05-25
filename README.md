2753bde2918118025755ac52de8db8dbf32da587
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-05-25 17:50:49 +0100

    Fix hardcode, rename variables

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # steam-cloud-and-autoupdate-disable
+Step 1: Open steam and enable cloud synchronisation in the steam settings
+Step 2: Close steam
+Step 3: Run
--- cloud_disable.cpp
+++ cloud_disable.cpp
@@ -7,18 +7,24 @@ using namespace std;
 
 int main()
 {
-    string input_directory;
+    string sharedconfig_directory;
+    string library_directory;
 
     // C:\Program Files (x86)\Steam\userdata\<id>\7\remote is the directory in Windows
-    cout << "Enter directory: ";
-    getline(cin, input_directory);
-    replace(input_directory.begin(), input_directory.end(), '\\', '/'); // Windows uses \ instead of / for directories which need to be switched.
-    string file_directory = input_directory + "/sharedconfig.vdf";      // Creates directory to the main config file
+    cout << "Enter directory for sharedconfig.vdf: ";
+    getline(cin, sharedconfig_directory);
+    replace(sharedconfig_directory.begin(), sharedconfig_directory.end(), '\\', '/'); // Windows uses \ instead of / for directories which need to be switched.
+    string sharedconfig_file = sharedconfig_directory + "/sharedconfig.vdf";          // Creates directory to the main config file
 
     // Input stream class to operate on the library folders file which has all the game ids
-    ifstream library("C:/Program Files (x86)/Steam/steamapps/libraryfolders.vdf");
-    stringstream library_buffer;       // Create a stringstream object which can be used to create a buffer to store information in
-    library_buffer << library.rdbuf(); // Add the information from the library ifstream to the library buffer
+    cout << "Enter directory for libraryfolders.vdf: ";
+    getline(cin, library_directory);
+    replace(library_directory.begin(), library_directory.end(), '\\', '/');
+    string library_file = library_directory + "/libraryfolders.vdf";
+    ifstream library_if(library_file);
+
+    stringstream library_buffer;          // Create a stringstream object which can be used to create a buffer to store information in
+    library_buffer << library_if.rdbuf(); // Add the information from the library ifstream to the library buffer
 
     stringstream id_buffer;       // Create a stringstream object to store only the ids from the library_buffer
     string line;                  // String used to temporarily store each line of the library in the while loop
@@ -27,6 +33,7 @@ int main()
     // While loop to go through each line in the library
     while (getline(library_buffer, line))
     {
+
         // if the "apps" line is found (not equal to no position), then turn the bool switch on
         if (line.find("apps") != string::npos)
         {
@@ -62,14 +69,16 @@ int main()
     // Outputs the ids to the terminal
     cout << id_buffer.str() << endl;
 
-    ifstream if_file(file_directory); // Input file stream created for the main config
+
+
+    ifstream sharedconfig_if(sharedconfig_file); // Input file stream created for the main config
     stringstream config_buffer;       // Stringstream buffer which we will use to replace the original config
 
     line = "";
     bool appsBlockReached = false;
 
     // While loop to go through each line from the main config and add it to the config_buffer accordingly
-    while (getline(if_file, line))
+    while (getline(sharedconfig_if, line))
     {
         // If apps block has been reached
         if (line.find("apps") != string::npos)
@@ -107,10 +116,14 @@ int main()
     }
 
     // Output file stream created so that we can add the new config we have created and save it replacing the old config
-    ofstream outputFile(file_directory);
-    outputFile << config_buffer.str();
+    ofstream sharedconfig_of(sharedconfig_file);
+    sharedconfig_of << config_buffer.str();
 
     // Shows us the state of the config file after running this script
     cout << config_buffer.str() << endl;
+
+    library_if.close();
+    sharedconfig_if.close();
+    sharedconfig_of.close();
     return 0;
 }

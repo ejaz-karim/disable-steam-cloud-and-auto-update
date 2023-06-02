6ae3428a134a4a90fbb46d408e3fd13c21e8feaa
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-06-02 15:47:49 +0100

    Update autoupdate_disable.cpp functions

--- autoupdate_disable.cpp
+++ autoupdate_disable.cpp
@@ -2,9 +2,24 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <filesystem>
 #include "autoupdate_disable.hpp"
 #include "utility.hpp"
 
 using namespace std;
 
+bool checkUpdateBehaviour(const string &buffer)
+{
+    return buffer.find("AutoUpdateBehavior") != string::npos;
+}
 
+bool replaceUpdateBehaviour()
+{
+
+}
+
+string iterateSteamApps(const string &steamAppsDirectory)
+{
+    for(const auto& directory : )
+
+}
--- cloud_disable.cpp
+++ cloud_disable.cpp
@@ -117,37 +117,3 @@ bool replaceAppsBlock(const string &sharedConfigFile, const string &sharedConfig
 
     return true;
 }
-
-int main()
-{
-    try
-    {
-        string sharedconfig_directory = getDirectory("Enter directory for sharedconfig.vdf: ");
-        string sharedconfig_file = sharedconfig_directory + "/sharedconfig.vdf";
-
-        string library_directory = getDirectory("Enter directory for libraryfolders.vdf: ");
-        string library_file = library_directory + "/libraryfolders.vdf";
-
-        string sharedconfig_content = readFileContents(sharedconfig_file);
-        string library_content = readFileContents(library_file);
-
-        string game_ids = extractGameIds(library_content);
-
-        if (!game_ids.empty())
-        {
-            if (replaceAppsBlock(sharedconfig_file, sharedconfig_content, game_ids))
-            {
-                cout << "Success." << endl;
-            }
-        }
-
-        cout << "Press ENTER to exit..." << endl;
-        getchar();
-        return 0;
-    }
-    catch (const exception &e)
-    {
-        cerr << "Error: " << e.what() << endl;
-        return 1;
-    }
-}

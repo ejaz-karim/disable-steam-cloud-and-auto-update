1f7837fc24fb016f80aacb2a24a4ca6db50a977c
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-10-08 23:06:59 +0100

    add vcpkg.json

--- src/api.cpp
+++ src/api.cpp
@@ -5,16 +5,11 @@
 #include <filesystem>
 #include <string>
 #include "disable-steam-cloud-and-auto-update/api.hpp"
-// #include <Poco/Net/HTTPClientSession.h>
-// #include <Poco/Net/HTTPRequest.h>
-// #include <Poco/Net/HTTPResponse.h>
-// #include <Poco/StreamCopier.h>
-// #include <Poco/URI.h>
-// #include <Poco/JSON/Parser.h>
+
+#include <drogon/drogon.h>
 
 using namespace std;
 
-// Function that takes a string of gameIds and sends each line to the api
 string url = "https://store.steampowered.com/api/appdetails?appids=";
 
 stringstream removeQuotes(const string &game_ids){
@@ -28,35 +23,3 @@ stringstream removeQuotes(const string &game_ids){
     }
     return noQuotes;
 }
-
-// string getJSONBody(const string url) {
-//     Creating the client session
-//     Poco::URI uri(url);
-//     Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
-
-//     // Create the request
-//     Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET uri.getpath(), Poco::Net::HTTPRequest::HTTP_1_1);
-//     session.sendRequest(request);
-
-//     // Get the response
-//     Poco::Net::HTTPResponse response;
-//     std::istream& responseBody = session.receiveResponse(response);
-
-//     // Copy the response into a string
-//     std::string jsonBody;
-//     Poco::StreamCopier::copyToString(responseBody, jsonBody);
-//     return jsonBody;
-// }
-
-// string getGameName(string jsonBody) {
-    
-// }
-
-// void apiRequest(stringstream &appIds){
-//     string line;
-//     while(getline(appIds, line)){
-//         string temp = "";
-//         temp = url + line;
-//         cout << getJSONBody(temp) << endl;
-//     }
-// }

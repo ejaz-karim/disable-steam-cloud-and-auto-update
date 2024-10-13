13e7fcede12cc56e2f0842576c1f2dae5febc2d8
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2024-10-13 05:02:10 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,5 @@
-add_rules("mode.debug", "mode.release")
-
 target("disable-steam-cloud-and-auto-update")
     set_kind("binary")
-    add_includedirs("include/disable-steam-cloud-and-auto-update")
-    add_files("src/*.cpp")
+    add_includedirs("include")
+    add_headerfiles("include/**.hpp")
+    add_files("src/**.cpp")

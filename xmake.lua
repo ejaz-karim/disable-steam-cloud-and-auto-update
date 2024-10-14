4e64198899cf6f56cec369228d69f1bce0c70381
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2024-10-14 07:40:43 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,3 +3,7 @@ target("disable-steam-cloud-and-auto-update")
     add_includedirs("include")
     add_headerfiles("include/**.hpp")
     add_files("src/**.cpp")
+
+    if is_plat("windows") and is_tool("gcc") then
+        add_ldflags("-static", "-static-libgcc", "-static-libstdc++", {force = true})
+    end

153913c7695c9e4df1623decc1ee04778509f5d4
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2024-11-04 07:51:59 +0000

    Add main logic for automatic cloud disabling

--- src/main.cpp
+++ src/main.cpp
@@ -27,38 +27,48 @@ int main()
                 CloudDisabler cloudDisabler;
                 Api api;
 
-                string userDataPath = "C:\\Program Files (x86)\\Steam\\userdata";
-                string steamAppsPath = "C:\\Program Files (x86)\\Steam\\steamapps";
+                string userDataPath = "C:/Program Files (x86)/Steam/userdata";
+                string steamAppsPath = "C:/Program Files (x86)/Steam/steamapps";
                 if (fileUtility.checkPathExists(userDataPath) && fileUtility.checkPathExists(steamAppsPath))
                 {
                     string library_file = steamAppsPath + "/libraryfolders.vdf";
                     string library_content = fileUtility.readFileContents(library_file);
                     string game_ids = cloudDisabler.extractGameIds(library_content);
 
-                    for(const auto& entry : filesystem::directory_iterator(userDataPath)){
-
-
-                    }
-
-
-
-
-
-                    if (!game_ids.empty())
+                    for (const auto &entry : filesystem::directory_iterator(userDataPath))
                     {
-                        if (cloudDisabler.replaceAppsBlock(sharedconfig_file, sharedconfig_content, game_ids))
+                        if (entry.is_directory())
                         {
-                            cout << ">Success" << endl;
+                            string steamID = entry.path().string();
+                            string remotePath = steamID + "/7/remote";
+                            if (filesystem::exists(remotePath))
+                            {
+                                string sharedconfig_file = remotePath + "/sharedconfig.vdf";
+                                string sharedconfig_content = fileUtility.readFileContents(sharedconfig_file);
+
+                                if (!game_ids.empty())
+                                {
+                                    if (cloudDisabler.replaceAppsBlock(sharedconfig_file, sharedconfig_content, game_ids))
+                                    {
+                                        cout << ">Success" << endl;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                cout << "Could not find /remote/ folder for this Steam ID" << endl;
+                            }
                         }
                     }
+
                     break;
                 }
                 else
                 {
-                    string sharedconfig_directory = fileUtility.getDirectory("\n>Enter directory for sharedconfig.vdf:\nExample: C:\\Program Files (x86)\\Steam\\userdata\\STEAM ID\\7\\remote");
+                    string sharedconfig_directory = fileUtility.getDirectory("\n>Enter directory for sharedconfig.vdf:\nExample: C:/Program Files (x86)/Steam/userdata/STEAM ID/7/remote");
                     string sharedconfig_file = sharedconfig_directory + "/sharedconfig.vdf";
 
-                    string library_directory = fileUtility.getDirectory("\n>Enter directory for libraryfolders.vdf:\nExample: C:\\Program Files (x86)\\Steam\\steamapps");
+                    string library_directory = fileUtility.getDirectory("\n>Enter directory for libraryfolders.vdf:\nExample: C:/Program Files (x86)/Steam/steamapps");
                     string library_file = library_directory + "/libraryfolders.vdf";
 
                     string sharedconfig_content = fileUtility.readFileContents(sharedconfig_file);
@@ -84,7 +94,7 @@ int main()
                 FileUtility fileUtility;
                 AutoUpdateDisabler autoUpdateDisabler;
 
-                string steamAppsPath = "C:\\Program Files (x86)\\Steam\\steamapps";
+                string steamAppsPath = "C:\\Program Files (x86)/Steam/steamapps";
 
                 if (fileUtility.checkPathExists(steamAppsPath))
                 {
@@ -98,7 +108,7 @@ int main()
                 {
                     cout << "Could not find Steam directory in Program Files (x86)\n"
                          << endl;
-                    string manualSteamAppsPath = fileUtility.getDirectory(">Enter directory for \\Steam\\steamapps\\ :\n");
+                    string manualSteamAppsPath = fileUtility.getDirectory(">Enter directory for /Steam/steamapps/ :\n");
                     bool iterateManualPath = autoUpdateDisabler.iterateSteamApps(manualSteamAppsPath);
                     if (iterateManualPath)
                     {
